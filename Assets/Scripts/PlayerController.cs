25dec10aa320ef30bad7f69d2fb983196ab83dda
cliffajefo <cliffajefo@gmail.com>
2023-01-14 21:49:44 +0100

    ShrinkingPlanet

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float rotationSpeed = 10f;
+
+	public float rotation;
+
+	public float xrotation;
+	public float yrotation;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	
+	
+   
+
+
+	void Update ()
+	{
+		//rotation = Input.GetAxisRaw("Horizontal");
+		
+
+	}
+
+	
+
+	void FixedUpdate ()
+	{
+		rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+		
+		/*
+		Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
+		Quaternion deltaRotation = Quaternion.Euler(yRotation);
+		Quaternion targetRotation = rb.rotation * deltaRotation;
+		rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
+		//transform.Rotate(0f, rotation * rotationSpeed * Time.fixedDeltaTime, 0f, Space.Self);
+
+		*/
+	}
+	
+	public void moveLeft()
+    {
+		rotation = -1;
+
+		Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
+		Quaternion deltaRotation = Quaternion.Euler(yRotation);
+		Quaternion targetRotation = rb.rotation * deltaRotation;
+		rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
+		//transform.Rotate(0f, rotation * rotationSpeed * Time.fixedDeltaTime, 0f, Space.Self);
+
+	}
+
+	public void moveRight()
+    {
+		rotation = 1;
+		Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
+		Quaternion deltaRotation = Quaternion.Euler(yRotation);
+		Quaternion targetRotation = rb.rotation * deltaRotation;
+		rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
+		//transform.Rotate(0f, rotation * rotationSpeed * Time.fixedDeltaTime, 0f, Space.Self);
+
+	}
+
+
+
+}
